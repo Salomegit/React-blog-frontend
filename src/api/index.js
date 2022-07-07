9f1021d8939f0b5138aa8b644a0fd52df67608e8
salome <sallygithinji72@gmail.com>
2022-07-07 18:33:38 +0300

    update base url

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const config={
-     BASED_URL:process.env.REACT_APP_BACKEND_URL
+const config = {
+    BASED_URL: process.env.REACT_APP_BACKEND_URL,
 }
-const axiosQuery = axios.create({ baseURL:config.BASED_URL });
-export { axiosQuery,config as reactConfig };
+const axiosQuery = axios.create({ baseURL: config.BASED_URL + "/backend" });
+export { axiosQuery, config as reactConfig };
