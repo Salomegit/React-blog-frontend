01bdec351b60353d64637cfe5b671a29a286c1bd
salome <sallygithinji72@gmail.com>
2022-07-07 18:05:52 +0300

    add backend url for api  calls

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const BASED_URL = "http://localhost:5000/backend";
+const BASED_URL =process.env.REACT_APP_BACKEND_URL;
 const axiosQuery = axios.create({ baseURL: BASED_URL });
 export { axiosQuery };
